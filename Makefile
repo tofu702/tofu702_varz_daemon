2c2bd0d46d5e8e37d9b490a36607c415575e1dd5
Steven Stanek <steven@shopkick.com>
2015-02-22 21:18:07 -0800

    Support for benchmark

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 CC=gcc
 CFLAGS=-Wall -std=c99 -g
+BENCH_CFLAGS=-O3 -std=c99
 THIRDPARTY_SOURCES=sds/sds.c
 SOURCES=$(THIRDPARTY_SOURCES) counter_variable.c time_utils.c sample_variable.c hash_table.c json_helpers.c input_parser.c random.c hash_funcs.c executor.c
 TEST_SOURCES=$(SOURCES) counter_variable_test.c sample_variable_test.c executor_test.c hash_table_test.c json_helpers_test.c input_parser_test.c all_tests.c
+BENCH_SOURCES=$(SOURCES) benchmark.c
 TEST_FLAGS=-D VARZ_STUB
 DAEMON_SOURCES=$(SOURCES) daemon.c
 
@@ -11,8 +13,14 @@ build: build_test build_daemon
 build_daemon: $(DAEMON_SOURCES)
 	$(CC) -o daemon $(DAEMON_SOURCES) $(CFLAGS)
 
+build_bench: $(BENCH_SOURCES)
+	$(CC) -o bench $(BENCH_SOURCES) $(BENCH_CFLAGS)
+
 build_test: $(TEST_SOURCES)
 	$(CC) $(TEST_FLAGS) -o test $(TEST_SOURCES) $(CFLAGS)
 
 test: build_test
 	./test
+
+bench: build_bench
+	./bench
---
+++ benchmark.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "executor.h"
+#include "random.h"
+#include "time_utils.h"
+
+#define BENCHMARK_HT_SIZE 1024
+#define BENCHMARK_NUM_VAR_NAMES 512
+#define BENCHMARK_START_TIME VARZMakeTime(12, 1, 5, 0)
+#define BENCHMARK_END_TIME VARZMakeTime(12, 2, 5, 0)
+#define BENCHMARK_COUNTER_MAX_AMT 10
+
+/***** STATIC HELPER PROTOTYPES *****/
+static void generateRandomTime(varz_time_t *dest, varz_time_t start_time, varz_time_t end_time);
+static struct VARZOperationDescription randomMHTCounterAddOp();
+static struct VARZOperationDescription randomMHTSamplerAddOp();
+static struct VARZOperationDescription allDumpJsonOp();
+static void benchmark_executor();
+
+
+int main(int argc, char **argv) {
+  benchmark_executor();
+  return 0;
+}
+
+
+/***** STATIC HELPERS *****/
+static void generateRandomVariableName(char *dest, int num_names) {
+  sprintf(dest, "variable_%d", rand() % num_names);
+}
+
+static void generateRandomTime(varz_time_t *dest, varz_time_t start_time, varz_time_t end_time) {
+  varz_time_t time_delta;
+  time_delta = end_time - start_time;
+  *dest = rand() % time_delta + start_time;
+}
+
+static struct VARZOperationDescription randomMHTCounterAddOp() {
+  struct VARZOperationDescription desc;
+
+  desc.op = VARZOP_MHT_COUNTER_ADD;
+  generateRandomVariableName(desc.variable_name, BENCHMARK_NUM_VAR_NAMES);
+  generateRandomTime(&(desc.op_data.counter_add_op.time), BENCHMARK_START_TIME, BENCHMARK_END_TIME);
+  desc.op_data.counter_add_op.amt = rand() % BENCHMARK_COUNTER_MAX_AMT;
+
+  return desc;
+}
+
+static struct VARZOperationDescription randomMHTSamplerAddOp() {
+  struct VARZOperationDescription desc;
+
+  desc.op = VARZOP_MHT_SAMPLE_ADD;
+  generateRandomVariableName(desc.variable_name, BENCHMARK_NUM_VAR_NAMES);
+  generateRandomTime(&(desc.op_data.sampler_add_op.time), BENCHMARK_START_TIME, BENCHMARK_END_TIME);
+  desc.op_data.sampler_add_op.value = rand();  // Doesn't matter at all
+  desc.op_data.sampler_add_op.random_vals[0] = VARZRand64();
+  desc.op_data.sampler_add_op.random_vals[1] = VARZRand64();
+
+  return desc;
+}
+
+static struct VARZOperationDescription allDumpJsonOp() {
+  struct VARZOperationDescription desc;
+  desc.op = VARZOP_ALL_DUMP_JSON;
+  return desc;
+}
+
+static void benchmark_executor() {
+  // Benchmark Only Adds
+  VARZExecutor_t executor;
+  struct VARZOperationDescription op;
+  double start_time, end_time;
+  char *json_dump;
+
+  const int num_commmands = 16*1024*1024;
+
+  VARZExecutorInit(&executor, BENCHMARK_HT_SIZE);
+
+  start_time = VARZCurrentDoubleTime();
+  for (int i=0; i < num_commmands; i++) {
+    switch (rand() % 2) {
+      case 0:
+        op = randomMHTCounterAddOp();
+        break;
+      default:
+        op = randomMHTSamplerAddOp();
+        break;
+    }
+    VARZExecutorExecute(&executor, &op);
+  }
+  end_time = VARZCurrentDoubleTime();
+  
+  printf("Executor Benchmark started at: %f, ended at: %f, elapsed: %f\n", start_time, end_time,
+         end_time-start_time);
+  
+  start_time = VARZCurrentDoubleTime();
+  op = allDumpJsonOp();
+  json_dump = (char *) VARZExecutorExecute(&executor, &op);
+  end_time = VARZCurrentDoubleTime();
+  
+  printf("JSON Dump Benchmark started at: %f, ended at: %f, elapsed: %f, len=%lu\n", start_time, 
+         end_time, end_time-start_time, strlen(json_dump));
+  free(json_dump);
+
+
+  VARZExecutorFree(&executor);
+
+
+}
